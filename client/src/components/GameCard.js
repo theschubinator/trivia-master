d9ce7e76bef31fb8b0d64dad90f47513694b25b6
github username <github email address>
2018-02-04 13:27:48 -0600

    questions option during a game are randomized

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { shuffle } from '../helpers/questions';
 
-export const GameCard = ({question}) => {
-	// debugger
+export const GameCard = ({question}) => {	
+	const shuffledKeys = shuffle(Object.keys(question).slice(2,6))
 	return (
 		<div>
 			<h2>{question.question}</h2>
-			<p>{question.answer}</p>
-			<p>{question.incorrect1}</p>
-			<p>{question.incorrect2}</p>
-			<p>{question.incorrect3}</p>
+			<p>{question[shuffledKeys[0]]}</p>
+			<p>{question[shuffledKeys[1]]}</p>
+			<p>{question[shuffledKeys[2]]}</p>
+			<p>{question[shuffledKeys[3]]}</p>
 		</div>
 	)
 }
--- client/src/helpers/questions.js
+++ client/src/helpers/questions.js
@@ -8,3 +8,18 @@ export const randomQuestion = (questions) => {
 	const randomIndex = Math.floor(Math.random() * questions.length)
 	return questions[randomIndex]
 }
+
+export const shuffle = (array) => {
+	  let currentIndex = array.length, temporaryValue, randomIndex;
+	  // While there remain elements to shuffle...
+	  while (currentIndex !== 0) {
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+	  return array;
+};
