f523a73101068b5aeebbbf24d9f6e49d60ef1204
github username <github email address>
2018-02-06 17:42:43 -0600

    changed poxy and cors locations

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'https://trivia-master.herokuapp.com/'
 
     resource '*',
       headers: :any,
