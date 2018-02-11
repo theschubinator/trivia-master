d650be6ef15ddf5853c3785cfd55e3f22d7b3b10
github username <github email address>
2018-02-10 21:51:21 -0600

    user can sort questions by category in ShowQuestion.js

--- client/src/containers/ShowQuestions.js
+++ client/src/containers/ShowQuestions.js
@@ -1,44 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import QuestionCard from '../components/QuestionCard';
 import { Button } from 'react-bootstrap';
 
-const showQuestions = ({user, questions}) => {
-	if(!user.loggedIn) return <Redirect to="/" />
-  
-  const permittedQuestions = () => {
-		if (!user.admin) {
-			let userQuestions = questions.filter(question => question.user.username === user.username)
-			return userQuestions;
-		} else {
-			return questions
+class ShowQuestions extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			category: 'All'
 		}
+	}
+	
+	handleClick = (e) => {
+		this.setState({ category: e.target.name})
 	};
 
-	const displayQuestions = permittedQuestions().map((question, i) => {
+	render() {
+		if(!this.props.user.loggedIn) return <Redirect to="/" />
+
+		const permittedQuestions = () => {
+			if (!this.props.user.admin) {
+				let userQuestions = this.props.questions.filter(question => question.user.username === this.props.user.username)
+				return filteredQuestionsByCategory(userQuestions);
+			} 
+			return filteredQuestionsByCategory(this.props.questions)
+		};
+
+		const filteredQuestionsByCategory = (questions) => {
+			if(this.state.category !== 'All') {
+				return questions.filter(question => question.category.name === this.state.category)
+			}
+			return questions
+		};
+
+		const displayQuestions = permittedQuestions().map((question, i) => {
+			return (
+				<div className="col-md-6" key={i}>
+					<QuestionCard question={question} />
+				</div>
+			)
+		})
+
 		return (
-			<div className="col-md-6" key={i}>
-				<QuestionCard question={question} />
+			<div className="row">
+			<Button name="All" onClick={this.handleClick}>All</Button>
+			<Button name="Sports" onClick={this.handleClick}>Sports</Button>	
+			<Button name="History" onClick={this.handleClick}>History</Button>
+			<Button name="Science" onClick={this.handleClick}>Science</Button>
+			<Button name="Entertainment" onClick={this.handleClick}>Entertainment</Button>
+				<p id="questionNote"><b>Note: </b><i>Green signifies the correct answer.</i></p>
+				{displayQuestions}
 			</div>
 		)
-	})
-	
-	const handleClick = (e) => {
-		alert(e.target.name)
 	}
-
-	return (
-		<div className="row">
-		<Button name="All" onClick={handleClick}>All</Button>
-		<Button name="Sports" onClick={handleClick}>Sports</Button>	
-		<Button name="History" onClick={handleClick}>History</Button>
-		<Button name="Science" onClick={handleClick}>Science</Button>
-		<Button name="Entertainment" onClick={handleClick}>Entertainment</Button>
-			<p id="questionNote"><b>Note: </b><i>Green signifies the correct answer.</i></p>
-			{displayQuestions}
-		</div>
-	)
 }
 
 const mapStateToProps = (state) => {
@@ -48,4 +63,4 @@ const mapStateToProps = (state) => {
 	}
 }
 
-export default connect(mapStateToProps)(showQuestions);
+export default connect(mapStateToProps)(ShowQuestions);
