41058e0ad13b28c4472605924c0f4c6b17762213
github username <github email address>
2018-02-13 13:09:04 -0600

    try bootstrap on login page again

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -35,18 +35,24 @@ class Login extends Component {
 
 	render () {
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<label>Username:&nbsp;</label>
-				<input type="text" className="input-field" name="username" value={this.state.username} 
-				  onChange={this.handleChange} />
-				<label>Password:&nbsp;</label>
-				<input type="password" className="input-field" name="password" value={this.state.password} 
-				  onChange={this.handleChange} />
-				<Button bsStyle="warning" type="submit" block>LOGIN</Button>
-				<div className="row errors">
-					{this.state.error}
-				</div>	
-			</form>
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-12">
+						<form onSubmit={this.handleSubmit}>
+							<label>Username:&nbsp;</label>
+							<input type="text" className="input-field" name="username" value={this.state.username} 
+							  onChange={this.handleChange} />
+							<label>Password:&nbsp;</label>
+							<input type="password" className="input-field" name="password" value={this.state.password} 
+							  onChange={this.handleChange} />
+							<Button bsStyle="warning" type="submit" block>LOGIN</Button>
+							<div className="row errors">
+								{this.state.error}
+							</div>	
+						</form>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
